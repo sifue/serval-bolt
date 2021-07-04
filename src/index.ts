74659bcf797750a03ed369afba9db6004f517e6d
Soichiro Yoshimura <yoshimura@soichiro.org>
2021-07-04 20:33:55 +0900

    いいねカウントのインクリメント/デクリメントに対応

--- src/index.ts
+++ src/index.ts
@@ -16,61 +16,13 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-// グローバルショートカット
-app.shortcut('socket-mode-shortcut', async ({ ack, body, client }) => {
-  await ack();
-  await client.views.open({
-    trigger_id: body.trigger_id,
-    view: {
-      type: 'modal',
-      callback_id: 'modal-id',
-      title: {
-        type: 'plain_text',
-        text: 'タスク登録',
-      },
-      submit: {
-        type: 'plain_text',
-        text: '送信',
-      },
-      close: {
-        type: 'plain_text',
-        text: 'キャンセル',
-      },
-      blocks: [
-        {
-          type: 'input',
-          block_id: 'input-task',
-          element: {
-            type: 'plain_text_input',
-            action_id: 'input',
-            multiline: true,
-            placeholder: {
-              type: 'plain_text',
-              text: 'タスクの詳細・期限などを書いてください',
-            },
-          },
-          label: {
-            type: 'plain_text',
-            text: 'タスク',
-          },
-        },
-      ],
-    },
-  });
-});
-
-app.view('modal-id', async ({ ack, view, logger }) => {
-  console.log(view.state.values);
-  logger.info(`Submitted data: ${view.state.values}`);
-  await ack();
-});
-
-// イベント API
-app.message('こんにちは', async ({ message, say }) => {
+// 動作確認用 ping コマンド
+app.message('ping serval-bolt', async ({ message, say }) => {
   const m = message as GenericMessageEvent;
-  await say(`:wave: こんにちは <@${m.user}>！`);
+  await say(`pong <@${m.user}>`);
 });
 
+// いいねいくつ
 app.message('いいねいくつ', async ({ message, say }) => {
   const m = message as GenericMessageEvent;
   const record = await prisma.goodcounts.findUnique({
@@ -80,7 +32,7 @@ app.message('いいねいくつ', async ({ message, say }) => {
   await say(`<@${m.user}>ちゃんのいいねは ${goodcount} こだよ！`);
 });
 
-// リアクションに対する対応
+// リアクション追加に対する対応
 app.event('reaction_added', async ({ event, client }) => {
   const i = event.item as ReactionMessageItem;
   const itemUserId = event.item_user;
@@ -90,12 +42,10 @@ app.event('reaction_added', async ({ event, client }) => {
   const itemTs = i.ts;
   const eventTs = event.event_ts;
 
-  // DBへの保存
-  const record = await prisma.goodcounts.findUnique({
-    where: { userId: event.item_user },
-  });
-  const goodcount = record ? record.goodcount + 1 : 1;
-  await prisma.goodcounts.create({ data: { userId: itemUserId, goodcount } });
+  if (event.reaction !== '+1') return; // いいね以外を除外
+  // if (itemUserId === reactionUserId) return; // セルフいいねを除外 // TODO 戻す
+
+  // Goodreactionsへの保存
   await prisma.goodreactions.create({
     data: {
       itemUserId,
@@ -107,14 +57,69 @@ app.event('reaction_added', async ({ event, client }) => {
     },
   });
 
-  // レスポンス
-  await client.chat.postMessage({
-    channel: i.channel,
-    text: `:wave: リアクションありがとう reaction: :${event.reaction}: user: ${event.user} item_user: ${event.item_user} event_ts: ${event.event_ts}`,
+  // Goodcountsのインクリメント
+  const record = await prisma.goodcounts.findUnique({
+    where: { userId: event.item_user },
   });
+  const goodcount = record ? record.goodcount + 1 : 1;
+  if (record) {
+    await prisma.goodcounts.update({
+      where: { userId: itemUserId },
+      data: { goodcount },
+    });
+  } else {
+    await prisma.goodcounts.create({ data: { userId: itemUserId, goodcount } });
+  }
+
+  console.log(
+    `[INFO] Add Goodreaction goodcount: ${goodcount} itemUserId: ${itemUserId} reactionUserId: ${reactionUserId} eventTs: ${eventTs}`
+  );
+});
+
+// リアクション削除に対する対応
+app.event('reaction_removed', async ({ event, client }) => {
+  const i = event.item as ReactionMessageItem;
+  const itemUserId = event.item_user;
+  const reactionUserId = event.user;
+  const itemChannel = i.channel;
+  const itmeType = i.type;
+  const itemTs = i.ts;
+  const eventTs = event.event_ts;
+
+  if (event.reaction !== '+1') return; // いいね以外を除外
+  // if (itemUserId === reactionUserId) return; // セルフいいねを除外 // TODO 戻す
+
+  // Goodreactionsの削除
+  await prisma.goodreactions.deleteMany({
+    where: {
+      itemUserId,
+      reactionUserId,
+      itemChannel,
+      itmeType,
+      itemTs,
+    },
+  });
+
+  // Goodcountsのデクリメント
+  const record = await prisma.goodcounts.findUnique({
+    where: { userId: event.item_user },
+  });
+  const goodcount = record && record.goodcount > 0 ? record.goodcount - 1 : 0;
+  if (record) {
+    await prisma.goodcounts.update({
+      where: { userId: itemUserId },
+      data: { goodcount },
+    });
+  } else {
+    await prisma.goodcounts.create({ data: { userId: itemUserId, goodcount } });
+  }
+
+  console.log(
+    `[INFO] Remove Goodreaction goodcount: ${goodcount} itemUserId: ${itemUserId} reactionUserId: ${reactionUserId} eventTs: ${eventTs}`
+  );
 });
 
 (async () => {
   await app.start();
-  console.log('⚡️ Bolt app started');
+  console.log('[INFO] ⚡️ Bolt app started');
 })();
